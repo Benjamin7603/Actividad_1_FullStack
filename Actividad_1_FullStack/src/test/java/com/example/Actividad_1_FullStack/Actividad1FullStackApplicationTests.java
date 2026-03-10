0351307c02ee5c8120af1aeda38a7f12ea472565
Benjamin7603 <bgdiaz2006@gmail.com>
2026-03-10 17:23:53 -0300

    Inicio del proyecto

---
+++ Actividad_1_FullStack/src/test/java/com/example/Actividad_1_FullStack/Actividad1FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Actividad_1_FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad1FullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
